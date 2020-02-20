d36404ee3cf226312f1e965dafee941ec2bfe8b9
AdamQuiz <57140431+AdamQuiz@users.noreply.github.com>
2020-02-20 13:58:10 -0500

    End Scene Transitions to Start Menu /Quit Button Implemented

---
+++ ProdTechProject/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+   public void PlayAgain()
+    {
+        SceneManager.LoadScene("StartScene"); // loads main menu
+    }
+
+    public void Quit()
+    {
+        Application.Quit(); // quits the application
+
+    }
+}
