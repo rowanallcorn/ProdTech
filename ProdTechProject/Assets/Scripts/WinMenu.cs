465f62c40a75c2cd2b9fd23741dadca86b3d1e41
Rowan Allcorn <rowanallcorn@gmail.com>
2020-04-09 21:51:48 -0400

    Numbers working on end and lose scenes

--- ProdTechProject/Assets/Scripts/WinMenu.cs
+++ ProdTechProject/Assets/Scripts/WinMenu.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class WinMenu : MonoBehaviour
 {
-   public void PlayAgain()
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            PlayAgain();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Quit();
+        }
+    }
+
+    public void PlayAgain()
     {
         SceneManager.LoadScene("StartScene"); // loads main menu
     }
@@ -15,4 +28,5 @@ public class WinMenu : MonoBehaviour
         Application.Quit(); // quits the application
 
     }
+
 }
